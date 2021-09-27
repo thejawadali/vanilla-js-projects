fda00676c9b11783165959ee5af8174bb88e6341
Jawad Ali <alijamil734@gmail.com>
2021-09-27 22:15:45 +0500

    fix: make  search bar responsive for mobile devices and hide on esc

--- 06-search-field/index.js
+++ 06-search-field/index.js
@@ -1,10 +1,18 @@
-const searchBar = document.getElementsByClassName("search")[0]
-var body = document.getElementsByTagName("BODY")[0];
-const searchBtn = document.getElementsByClassName("icon")[0]
-searchBtn.onclick = ()=>{
-  searchBar.classList.toggle("active")
+const searchBar = document.getElementsByClassName( "search" )[0]
+var body = document.getElementsByTagName( "BODY" )[0]
+const searchBtn = document.getElementsByClassName( "icon" )[0]
+searchBtn.onclick = () => {
+  searchBar.classList.toggle( "active" )
 }
 
-body.onclick = ()=>{
-  console.log("bod");
+body.onclick = ( event ) => {
+  if ( event.target.tagName === "BODY" && searchBar.classList.contains( "active" ) ) {
+    searchBar.classList.remove( "active" )
+  }
 }
+
+document.addEventListener( 'keydown', function ( event ) {
+  if ( event.key === "Escape" && searchBar.classList.contains( "active" ) ) {
+    searchBar.classList.remove( "active" )
+  }
+} )
